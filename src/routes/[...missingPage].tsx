f57e43c85b2dbf2633c2eba463370a4dcc702a33
Alex_Malv <a.malinovsky303@gmail.com>
2023-03-05 00:50:45 +0100

    routes and i lint

---
+++ src/routes/[...missingPage].tsx
@@ -0,0 +1,10 @@
+
+
+export default function MissingPage() {
+  return (
+    <div>
+      <h2>Oops...</h2>
+      <h3>Missing Page</h3>
+    </div>
+  )
+}
---
+++ src/routes/projectsLayout.tsx
@@ -0,0 +1,11 @@
+import {Outlet} from "@solidjs/router";
+
+export default function ProjectsLayout() {
+  return (
+    <div>
+      <h1>ProjectsLayout</h1>
+      <Outlet/>
+    </div>
+  )
+
+}
